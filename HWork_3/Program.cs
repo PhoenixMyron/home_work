380ea09b7c5a2470ece7428692e91a95ef86a113
PhoenixMyron <phoenixmyron@gmail.com>
2022-11-23 16:17:01 +0600

    added true or false

---
+++ HWork_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+
+double z = n % 2;
+if ( z == 0 )
+    Console.Write($"{true} ");
+else 
+    Console.Write($"{false} ");
